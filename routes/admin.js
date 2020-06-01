70cd4009e0616ef010722d83c179f92512997a3f
adMenon <adityaharidas.m16@iiits.in>
2020-06-01 19:11:00 +0530

    few APIs and models created

---
+++ routes/admin.js
@@ -0,0 +1,83 @@
+const express = require('express');
+
+const router = express.Router();
+
+const Bus = require("../models/Bus");
+
+
+
+
+
+
+var busId = 90000;
+
+router.post('/addBus', async(req, res)=>{
+    var noOfSeats = req.body.noOfSeats;
+    var busSeats = [];
+    for(var i = 1;i<=noOfSeats;i++){
+        busSeats.push(new Bus({
+            BusID:busId,
+            seatNo: i
+        }));
+    }
+    try{
+        const newBus = await Bus.insertMany(busSeats);
+        console.log(newBus)
+        res.send(newBus);
+        busId++;
+    }catch(err){
+        res.send("errr");
+        throw err;
+    }
+});
+router.get('/getBuses/:BusID', async(req,res)=>{
+    try{
+        const Buses = await Bus.find({BusID:req.params.BusID}).populate('PassengerDetails');
+        console.log(Buses);
+        res.json(Buses);
+    }
+    catch(err){
+        res.send(err);
+        throw err;
+    }
+});
+
+
+
+router.patch('/reset/:BusID', async(req,res)=>{
+    try{
+        const updateBus = await Bus.updateMany({BusID:req.params.BusID},
+            {"$set":{isBooked:false}, "$unset":{PassengerDetails:1, BookingID:1, PhoneNumber:1, DateOfBooking:1}});
+        console.log(updateBus+" \nTickets deleted, seats flushed");
+        res.json("Seats flushed");
+    }
+    catch(err){
+        res.send("err");
+        throw err;
+    }
+});
+
+router.delete('/deleteAll', async(req,res)=>{
+    try{
+        const removedBus = await Bus.remove({});
+        console.log(removedBus.deletedCount + " Bus(es) removed")
+        res.json(removedBus);
+    }
+    catch(err){
+        res.send({"error":err});
+    }
+});
+
+
+router.delete('/deleteBus', async(req,res)=>{
+    try{
+        const removedBus = await Bus.remove({_id:req.body.busId});
+        res.json(removedBus);
+    }
+    catch{
+        res.send({"error":err});
+    }
+});
+
+
+module.exports = router;
---
+++ routes/ticketStatus.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const router = express.Router();
+
+const Bus = require("../models/Bus");
+
+
+router.post('/viewTicket',async (req,res)=>{
+    try{
+        var bookingId = req.body.bookingId;
+        var phoneNumber = req.body.phoneNumber;
+        
+        const getTickets = await Bus.find({BookingId:bookingId, PhoneNumber: phoneNumber}).populate('PassengerDetails');
+        console.log(getTickets);
+        if(getTickets.length==0){
+            res.send("No tickets found");
+        }
+        res.json(getTickets);
+    }
+    catch(err){
+        res.send("err");
+        throw err;
+    }
+});
+router.delete('/deleteTicket', async (req,res)=>{
+    try{
+        console.log(req.body);
+        var bookingId = req.body.bookingId;
+        var seats = req.body.seats;
+        var busId = req.body.busId;
+        
+
+        
+        const updateBus = await Bus.updateMany({BusID:busId,Booking_ID:bookingId, seatNo: {$in : seats}},
+            {"$set":{isBooked:false}, "$unset":{PassengerDetails:1, BookingID:1, PhoneNumber:1, DateOfBooking:1}});
+        console.log(updateBus);
+        res.json(updateBus);
+    }
+    catch(err){
+        res.send("err");
+        throw err;
+    }
+});
+module.exports = router;
