8db7e7b61a04aee81077181cd0b5c77baef6e5c2
adMenon <adityaharidas.m16@iiits.in>
2020-06-03 18:18:14 +0530

    Bug fix 0.3

--- routes/userAuth.js
+++ routes/userAuth.js
@@ -82,10 +82,10 @@ router.delete('/deleteUser', async(req,res)=>{
             return res.status(404).json({Message:"No account found"});
         }
         console.log(deleteUser);
-        res.status(204).json("Deleted");
+        res.status(204).json({Message:"User Deleted"});
     }
     catch(err){
-        res.status(500).json("err");
+        res.status(500).json({Error:err}));
         throw err;
     }
     
